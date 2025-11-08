fa2964e528a06df5b7fda1b7c5f76f834b2a090b
{ydangnie} <nieydpk04105@gmail.com>
2025-11-08 10:10:25 +0700

    PredatorGroup

--- resources/views/admin/dasboard.blade.php
+++ resources/views/admin/dasboard.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard - Quản Lý Đồng Hồ</title>   
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/admin/admindasboard.css', 'resources/js/admin/dasboard.js'])
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
